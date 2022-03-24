59ad345d8e109550984aa19b0c0a0fea3ff0e0f1
mauduran <is716122@iteso.mx>
2022-03-24 00:31:58 -0600

    Refactored server.js and created mqtt_utils

--- MQTT_listener_server/server.js
+++ MQTT_listener_server/server.js
@@ -1,7 +1,7 @@
 const mqtt = require('async-mqtt');
 const MQTT_OPTIONS = require('./config/mqtt_options');
 const { SENSOR_TOPICS } = require('./constants/mqtt_topics');
-const { messageForwardingFunctions } = require('./utils/message_forwarding.utils');
+const {handleIncomingMessage} = require('./utils/mqtt_utils');
 
 if (process.env.NODE_ENV == 'dev') {
     require('dotenv').config();
@@ -20,32 +20,6 @@ const handleFailedMqttConnection = (err) => {
     console.log(err);
 }
 
-
-const handleIncomingMessage = (topic, payload) => {
-    console.log(`Incoming message to ${topic}`);
-
-    const topicSplit = topic.split('/');
-    const accountId = topicSplit[0];
-    const measurement = topicSplit[1]
-    try {
-        message = JSON.parse(payload.toString());
-
-        if (!message.measurement || !message.value || !message.accountPassword) {
-            console.log("Error. Invalid JSON payload.");
-        } else {
-            console.log(`AccountId ${accountId} - ${measurement}: ${message.value} ${message.measurement}`);
-            if (messageForwardingFunctions[measurement]) {
-                messageForwardingFunctions[measurement](accountId, message);
-            } else {
-                console.log("Invalid sensor measurement on topic.");
-            }
-        }
-    } catch (error) {
-        console.log(error);
-        console.log(`Error. Could not process ${measurement} payload sent to ${accountId}. Please send JSON message.`);
-    }
-
-}
 
 client.on('error', handleFailedMqttConnection);
 client.on('connect', handleSuccessfulMqttConnection);
---
+++ MQTT_listener_server/utils/mqtt_utils.js
@@ -0,0 +1,30 @@
+const { messageForwardingFunctions } = require('./utils/message_forwarding.utils');
+
+const handleIncomingMessage = (topic, payload) => {
+    console.log(`Incoming message to ${topic}`);
+
+    const topicSplit = topic.split('/');
+    const accountId = topicSplit[0];
+    const measurement = topicSplit[1]
+    try {
+        message = JSON.parse(payload.toString());
+
+        if (!message.measurement || !message.value || !message.accountPassword) {
+            console.log("Error. Invalid JSON payload.");
+        } else {
+            console.log(`AccountId ${accountId} - ${measurement}: ${message.value} ${message.measurement}`);
+            if (messageForwardingFunctions[measurement]) {
+                messageForwardingFunctions[measurement](accountId, message);
+            } else {
+                console.log("Invalid sensor measurement on topic.");
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        console.log(`Error. Could not process ${measurement} payload sent to ${accountId}. Please send JSON message.`);
+    }
+}
+
+module.exports = {
+    handleIncomingMessage,
+}
