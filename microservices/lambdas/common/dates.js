9eed54b10c169c423f06766a6be1d142d2d038fe
mauduran <is716122@iteso.mx>
2022-03-26 14:51:43 -0600

    Added get week number function to dates.js

--- microservices/lambdas/common/dates.js
+++ microservices/lambdas/common/dates.js
@@ -25,6 +25,15 @@ const getDateString = (isoDate) => {
     return `${day}/${month}/${date.getFullYear()}`;
 }
 
+const getWeekNumber = (d) => {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    var weekNum = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+    return { weekYear: d.getUTCFullYear(), weekNum: weekNum };
+}
+
+
 const addHours = (date, hours) => {
     let newDate = date
     newDate.setHours(date.getHours() + hours);
@@ -35,5 +44,6 @@ module.exports = {
     getTodayDate,
     getTodayString,
     addHours,
-    getDateString
+    getDateString,
+    getWeekNumber,
 }
