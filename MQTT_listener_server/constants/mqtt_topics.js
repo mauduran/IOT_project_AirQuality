41e24381df0f08bccb97860f44ce40844d7ca747
mauduran <is716122@iteso.mx>
2022-03-27 15:53:03 -0600

    Updated topic name for pm2.5

--- MQTT_listener_server/constants/mqtt_topics.js
+++ MQTT_listener_server/constants/mqtt_topics.js
@@ -1,4 +1,4 @@
-const SENSOR_TOPICS = ['+/co2', '+/pm25', '+/pm10', '+/temperature', '+/humidity', '+/voc'];
+const SENSOR_TOPICS = ['+/co2', '+/pm2.5', '+/pm10', '+/temperature', '+/humidity', '+/voc'];
 
 module.exports = {
     SENSOR_TOPICS,
