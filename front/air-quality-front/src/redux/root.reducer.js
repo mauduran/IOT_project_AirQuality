c8becf2b27fddacb65b372bf579edeba1b507ccf
mauduran <is716122@iteso.mx>
2022-04-13 00:21:06 -0500

    Added redux files for notifications

--- front/air-quality-front/src/redux/root.reducer.js
+++ front/air-quality-front/src/redux/root.reducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import accountReducer from './account/account.reducer';
+import notificationReducer from './notification/notification.reducer';
+
 
 const rootReducer = combineReducers({
     account: accountReducer,
+    notification: notificationReducer,
 });
 
 export default rootReducer;
--- front/air-quality-front/src/redux/root.saga.js
+++ front/air-quality-front/src/redux/root.saga.js
@@ -1,8 +1,10 @@
 import { all, call } from 'redux-saga/effects';
 import { accountSagas } from './account/account.sagas';
+import { notificationSagas } from './notification/notification.sagas';
 
 export default function* rootSaga() {
     yield all([
-        call(accountSagas), 
+        call(accountSagas),
+        call(notificationSagas),
     ]);
 }
