b0fbd2e2ac69ddbc8aaa21ac882486dd4d0811e0
mauduran <is716122@iteso.mx>
2022-04-15 17:38:09 -0500

    Fixed auth middlewares

--- front/air-quality-front/src/common/RequireAuth.jsx
+++ front/air-quality-front/src/common/RequireAuth.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { useLocation, Navigate } from "react-router-dom";
 import { createStructuredSelector } from 'reselect';
-import { selectAccountProfile } from '../redux/account/account.selectors';
+import { selectCurrentAccountName } from '../redux/account/account.selectors';
 
-const RequireAuth = ({ children, accountProfile }) => {
+const RequireAuth = ({ children, accountName }) => {
     let location = useLocation();
 
-    if (accountProfile) {
+    if (accountName) {
         return children;
     }
     return <Navigate to="/signin" state={{ from: location }} />
@@ -15,7 +15,7 @@ const RequireAuth = ({ children, accountProfile }) => {
 }
 
 const mapStateToProps = createStructuredSelector({
-    accountProfile: selectAccountProfile
+    accountName: selectCurrentAccountName
 })
 
 export default connect(mapStateToProps, null)(RequireAuth);
--- front/air-quality-front/src/common/RequireUnAuth.jsx
+++ front/air-quality-front/src/common/RequireUnAuth.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import { connect } from 'react-redux';
 import { Navigate, useLocation } from "react-router-dom";
 import { createStructuredSelector } from 'reselect';
-import { selectAccountProfile } from '../redux/account/account.selectors';
+import { selectCurrentAccountName } from '../redux/account/account.selectors';
 
-const RequireUnAuth = ({ accountProfile, children }) => {
+const RequireUnAuth = ({ accountName, children }) => {
     const location = useLocation();
 
     let to = (location.state && location.state.from && location.state.from.pathname) ? location.state.from.pathname : "/";
 
-    return (accountProfile) ? <Navigate to={to} /> : children;
+    return (accountName) ? <Navigate to={to} /> : children;
 }
 
 const mapStateToProps = createStructuredSelector({
-    accountProfile: selectAccountProfile,
+    accountName: selectCurrentAccountName,
 })
 
 export default connect(mapStateToProps, null)(RequireUnAuth);
