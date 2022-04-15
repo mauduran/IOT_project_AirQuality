01ff0a596d2dbd2cc5ea67a93979eedb50452e47
mauduran <is716122@iteso.mx>
2022-04-15 10:55:17 -0500

    Added constants files

---
+++ front/air-quality-front/src/constants/RiskLevelColors.js
@@ -0,0 +1,10 @@
+const RiskLevelColors = {
+    GOOD: "green-6",
+    MODERATE: "yellow-5",
+    UNHEALTHY_FOR_SENSITIVE_GROUPS: "orange-5",
+    UNHEALTHY: "volcano-6",
+    VERY_UNHEALTHY: "magenta-6",
+    HAZARDOUS: "red-9",
+}
+
+export default RiskLevelColors;
---
+++ front/air-quality-front/src/constants/SensorTypes.js
@@ -0,0 +1,10 @@
+const SENSOR_TYPES = {
+    "pm2.5": "PM2.5",
+    pm10: "PM10",
+    temperature: "TEMPERATURE",
+    voc: "VOC",
+    humidity: "HUMIDITY",
+    co2: "CO2",
+}
+
+export default SENSOR_TYPES;
