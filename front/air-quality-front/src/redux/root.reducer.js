09aaa4267d1779e652c945a7076c1f1f933e449b
mauduran <is716122@iteso.mx>
2022-04-15 10:55:01 -0500

    Updated root reducer and root saga

--- front/air-quality-front/src/redux/root.reducer.js
+++ front/air-quality-front/src/redux/root.reducer.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux';
 import accountReducer from './account/account.reducer';
 import notificationReducer from './notification/notification.reducer';
+import phoneReducer from './phone/phone.reducer';
 
 
 const rootReducer = combineReducers({
     account: accountReducer,
     notification: notificationReducer,
+    phone: phoneReducer,
 });
 
 export default rootReducer;
--- front/air-quality-front/src/redux/root.saga.js
+++ front/air-quality-front/src/redux/root.saga.js
@@ -1,10 +1,12 @@
 import { all, call } from 'redux-saga/effects';
 import { accountSagas } from './account/account.sagas';
 import { notificationSagas } from './notification/notification.sagas';
+import { phoneSagas } from './phone/phone.sagas';
 
 export default function* rootSaga() {
     yield all([
         call(accountSagas),
         call(notificationSagas),
+        call(phoneSagas),
     ]);
 }
