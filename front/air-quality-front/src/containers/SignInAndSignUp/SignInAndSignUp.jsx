126bbe1919b06eb45fd7ccb825fafa5cfe6e51e5
mauduran <is716122@iteso.mx>
2022-04-15 14:24:14 -0500

    Create base component for Account Access

---
+++ front/air-quality-front/src/containers/SignInAndSignUp/SignInAndSignUp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './SignInAndSignUp.css';
+
+const SignInAndSignUp = () => {
+    return (
+        <div className='sign-in-and-sign-up'>
+            <h1>sign In and Sign Up</h1>
+            {/* <SignIn/>
+            <SignUp/> */}
+        </div>
+    )
+}
+
+export default SignInAndSignUp;
