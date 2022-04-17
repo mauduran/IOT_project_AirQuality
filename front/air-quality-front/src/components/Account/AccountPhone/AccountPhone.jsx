c82fddf6b56e5eb54c7d15c53b5e1cce742f657c
mauduran <is716122@iteso.mx>
2022-04-17 00:44:28 -0500

    Added empty components for change password and account phone

---
+++ front/air-quality-front/src/components/Account/AccountPhone/AccountPhone.jsx
@@ -0,0 +1,15 @@
+import Title from 'antd/lib/typography/Title'
+import React from 'react'
+
+const AccountPhone = () => {
+  return (
+    <>
+    <Title style={{ padding: "25px 15px", margin: 0 }} level={3}>Phone Number</Title>
+    <div className='change-password'>
+        
+    </div>
+</>
+  )
+}
+
+export default AccountPhone
---
+++ front/air-quality-front/src/components/Account/ChangePasswordForm/ChangePasswordForm.jsx
@@ -0,0 +1,17 @@
+
+import Title from 'antd/lib/typography/Title';
+import React from 'react';
+import './ChangePasswordForm.css';
+
+const ChangePasswordForm = () => {
+    return (
+        <>
+            <Title style={{ padding: "25px 15px", margin: 0 }} level={3}>Change Password</Title>
+            <div className='change-password'>
+                
+            </div>
+        </>
+    )
+}
+
+export default ChangePasswordForm
