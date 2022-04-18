8f89bbeaf630f6dd45d1be3f7cee608bc5537321
mauduran <is716122@iteso.mx>
2022-04-17 23:32:00 -0500

    Added more properties to phone state in reducer

--- front/air-quality-front/src/redux/phone/phone.reducer.js
+++ front/air-quality-front/src/redux/phone/phone.reducer.js
@@ -5,7 +5,10 @@ const INITIAL_STATE = {
     error: null,
     successMessage: null,
     waitingForVerification: false,
+    registeringPhone: false,
+    verifyingCode: false,
 }
+
 const phoneReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case PhoneActionTypes.VERIFY_PHONE_SUCCESS:
@@ -14,7 +17,8 @@ const phoneReducer = (state = INITIAL_STATE, action) => {
                 number: action.payload.phoneNumber,
                 error: null,
                 successMessage: action.payload.message,
-                waitingForVerification: false
+                waitingForVerification: false,
+                verifyingCode: false,
             }
         case PhoneActionTypes.REGISTER_PHONE_SUCCESS:
             return {
@@ -22,7 +26,20 @@ const phoneReducer = (state = INITIAL_STATE, action) => {
                 number: null,
                 error: null,
                 successMessage: null,
-                waitingForVerification: true
+                waitingForVerification: true,
+                registeringPhone: false,
+            }
+
+        case PhoneActionTypes.REGISTER_PHONE_START:
+            return {
+                ...state,
+                registeringPhone: true,
+            }
+        
+        case PhoneActionTypes.VERIFY_PHONE_START:
+            return {
+                ...state,
+                verifyingCode: true,
             }
 
         case PhoneActionTypes.REGISTER_PHONE_FAILED:
@@ -32,7 +49,9 @@ const phoneReducer = (state = INITIAL_STATE, action) => {
                 error: action.payload,
                 successMessage: null,
                 number: null,
-                waitingForVerification: false
+                waitingForVerification: false,
+                registeringPhone: false,
+                verifyingCode: false,
             }
         default: return state;
     }
