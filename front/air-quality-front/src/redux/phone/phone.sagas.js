02f5ee256de077076266f03b04e29c4f844df8ed
mauduran <is716122@iteso.mx>
2022-04-17 23:32:19 -0500

    Fixed bug and added notifications on phone sagas

--- front/air-quality-front/src/redux/phone/phone.sagas.js
+++ front/air-quality-front/src/redux/phone/phone.sagas.js
@@ -3,12 +3,14 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 import PhoneActionTypes from './phone.types';
 
 import { registerPhoneFailed, registerPhoneSuccess, verifyPhoneFailed, verifyPhoneSuccess } from './phone.actions';
+import { notification } from 'antd';
 
-export function* registerPhone({ payload: phoneNumber }) {
+export function* registerPhone({ payload: { phoneNumber } }) {
     const body = { phoneNumber };
 
     try {
-        const response = yield fetch("/phone/register", {
+        console.log("fetching...?");
+        const response = yield fetch("/account/phone/register", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -22,18 +24,29 @@ export function* registerPhone({ payload: phoneNumber }) {
         if (!responseBody.success) {
             throw new Error(responseBody.message);
         }
-
+        notification.success({
+            key: "success registering phone number",
+            message: 'Success registering phone number.',
+            description: responseBody.message,
+            duration: 5,
+        });
         yield put(registerPhoneSuccess(responseBody.message));
     } catch (error) {
+        notification.error({
+            key: "unable to register phone number",
+            message: 'Could not register phone number.',
+            description: error.message,
+            duration: 5,
+        });
         yield put(registerPhoneFailed(error));
     }
 }
 
-export function* verifyPhone({ payload: phoneNumber, verificationCode }) {
+export function* verifyPhone({ payload: { phoneNumber, verificationCode } }) {
     const body = { phoneNumber, verificationCode };
 
     try {
-        const response = yield fetch("/phone/verify", {
+        const response = yield fetch("/account/phone/verify", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -47,10 +60,20 @@ export function* verifyPhone({ payload: phoneNumber, verificationCode }) {
         if (!responseBody.success) {
             throw new Error(responseBody.message);
         }
-
-        yield put(verifyPhoneSuccess({message: responseBody.message, phoneNumber}));
+        notification.success({
+            key: "success verifying phone number",
+            message: 'Phone verification successful!',
+            description: responseBody.message,
+            duration: 5,
+        });
+        yield put(verifyPhoneSuccess({ message: responseBody.message, phoneNumber }));
     } catch (error) {
-
+        notification.error({
+            key: "unable to verify phone number",
+            message: 'Could not verify phone number.',
+            description: error.message,
+            duration: 5,
+        });
         yield put(verifyPhoneFailed(error));
     }
 }
@@ -66,6 +89,6 @@ export function* onVerifyPhoneStart() {
 export function* phoneSagas() {
     yield all([
         call(onRegisterPhoneStart),
-        call(onRegisterPhoneStart),
+        call(onVerifyPhoneStart),
     ])
 }
