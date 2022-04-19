b7592f1323200a7ada08736b8983a5d0d58cd91d
mauduran <is716122@iteso.mx>
2022-04-19 18:48:07 -0500

    Created grid on dashboard

--- front/air-quality-front/src/containers/Dashboard/Dashboard.jsx
+++ front/air-quality-front/src/containers/Dashboard/Dashboard.jsx
@@ -1,11 +1,34 @@
+import { Col, Row } from 'antd';
 import React from 'react'
 import { connect } from 'react-redux';
 
 const Dashboard = () => {
+
     return (
         <div className="Dashboard">
             <h1 className="title">HOME</h1>
+            <Row gutter={[24, 24]} justify="space-evenly" style={{width: "95%", margin: "10px auto"}}>
+                <Col xs={24} sm={24} md={12} lg={8} xl={6}>
+                    <div style={{ background: "blue", height: "300px", width: "100%" }} />
+                </Col>
+                <Col xs={24} sm={24} md={12} lg={8} xl={6}>
+                    <div style={{ background: "blue", height: "300px", width: "100%" }} />
+                </Col>
+                <Col xs={24} sm={24} md={12} lg={8} xl={6}>
+                    <div style={{ background: "blue", height: "300px", width: "100%" }} />
+                </Col>
+                <Col xs={24} sm={24} md={12} lg={8} xl={6}>
+                    <div style={{ background: "blue", height: "300px", width: "100%" }} />
+                </Col>
+                <Col xs={24} sm={24} md={12} lg={8} xl={6}>
+                    <div style={{ background: "blue", height: "300px", width: "100%" }} />
+                </Col>
+                <Col xs={24} sm={24} md={12} lg={8} xl={6}>
+                    <div style={{ background: "blue", height: "300px", width: "100%" }} />
+                </Col>
+            </Row>
 
+        
         </div>
     )
 }
