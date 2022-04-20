b7ecba13ad3672dca8565dd5f8925b78f49ccb2c
mauduran <is716122@iteso.mx>
2022-04-19 23:19:01 -0500

    Added most-recent monitors for all sensors to the Dashboard

--- front/air-quality-front/src/containers/Dashboard/Dashboard.jsx
+++ front/air-quality-front/src/containers/Dashboard/Dashboard.jsx
@@ -2,45 +2,44 @@ import { Col, Row } from 'antd';
 import Title from 'antd/lib/typography/Title';
 import React from 'react';
 import { connect } from 'react-redux';
+
 import HumidityMonitor from '../../components/Monitors/HumidityMonitor/HumidityMonitor';
 import TemperatureMonitor from '../../components/Monitors/TemperatureMonitor/TemperatureMonitor';
+import CO2Monitor from '../../components/Monitors/CO2Monitor/CO2Monitor';
+import VOCMonitor from '../../components/Monitors/VOCMonitor/VOCMonitor';
 import { msToMinutes } from '../../utils/time.utils';
+import PM25Monitor from '../../components/Monitors/PM25Monitor/PM25Monitor';
+import PM10Monitor from '../../components/Monitors/PM10Monitor/PM10Monitor';
 
 const Dashboard = () => {
-    const fetchInterval = msToMinutes(1);
+    const fetchInterval = msToMinutes(5);
     return (
         <div className="Dashboard">
             <div className='page-header'>
                 <Title style={{ fontSize: "48px", margin: 0, color: "white" }} level={1}>Dashboard</Title>
             </div>
 
-            <div style={{ margin: "10px auto", width: "94%" }}>
-                <Row gutter={[24, 24]} justify="space-evenly">
+            <div style={{ margin: "20px auto", width: "94%" }}>
+                <Row gutter={[24, 24]} style={{marginBottom: "24px"}} justify="space-evenly">
                     <Col xs={24} sm={24} md={12} lg={8} xl={6}>
                         <TemperatureMonitor delay={fetchInterval} />
                     </Col>
                     <Col xs={24} sm={24} md={12} lg={8} xl={6}>
                         <HumidityMonitor delay={fetchInterval} />
                     </Col>
+                </Row>
+                <Row gutter={[24, 24]} justify="space-evenly">
                     <Col xs={24} sm={24} md={12} lg={8} xl={6}>
-                        <div style={{ background: "blue", height: "300px", width: "100%" }}>
-                            CO2
-                        </div>
+                        <CO2Monitor delay={fetchInterval} />
                     </Col>
                     <Col xs={24} sm={24} md={12} lg={8} xl={6}>
-                        <div style={{ background: "blue", height: "300px", width: "100%" }}>
-                            VOC
-                        </div>
+                        <VOCMonitor delay={fetchInterval} />
                     </Col>
                     <Col xs={24} sm={24} md={12} lg={8} xl={6}>
-                        <div style={{ background: "blue", height: "300px", width: "100%" }}>
-                            PM2.5
-                        </div>
+                        <PM25Monitor delay={fetchInterval} />
                     </Col>
                     <Col xs={24} sm={24} md={12} lg={8} xl={6}>
-                        <div style={{ background: "blue", height: "300px", width: "100%" }}>
-                            PM10
-                        </div>
+                        <PM10Monitor delay={fetchInterval} />
                     </Col>
                 </Row>
             </div>
