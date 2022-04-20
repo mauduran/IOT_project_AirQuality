44fa31d00b0099b08e8463b14be6f70e21f87cca
mauduran <is716122@iteso.mx>
2022-04-19 23:05:38 -0500

    Added VOC Monitor

---
+++ front/air-quality-front/src/components/Monitors/VOCMonitor/VOCMonitor.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { useInterval } from '../../../hooks/useInterval';
+import { fetchingLastSensorData, selectLastSensorData } from '../../../redux/sensors/sensors.selectors';
+import { getLastSensorDataStart } from '../../../redux/sensors/sensors.actions';
+import SensorLevel from '../../../common/SensorLevel/SensorLevel';
+
+const VOCMonitor = ({ delay, voc, isLoading, fetchLastVOC }) => {
+    useEffect(() => {
+        fetchLastVOC();
+    }, [fetchLastVOC])
+
+    useInterval(
+        () => fetchLastVOC(),
+        delay
+    );
+
+    return (
+        <SensorLevel
+            sensorType="VOC"
+            sensorData={voc}
+            isLoading={isLoading} />
+    )
+}
+
+const mapDispatchToProps = dispatch => ({
+    fetchLastVOC: () => dispatch(getLastSensorDataStart("voc"))
+})
+
+const mapStateToProps = createStructuredSelector({
+    voc: selectLastSensorData("voc"),
+    isLoading: fetchingLastSensorData("voc"),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(VOCMonitor);
