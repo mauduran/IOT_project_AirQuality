3a12106dc8bf543a9a4c91740f86870fee8321fe
mauduran <is716122@iteso.mx>
2022-04-19 21:25:18 -0500

    Added HumidityMonitor to Dashboard

--- front/air-quality-front/src/containers/Dashboard/Dashboard.jsx
+++ front/air-quality-front/src/containers/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import { Col, Row } from 'antd';
 import Title from 'antd/lib/typography/Title';
 import React from 'react';
 import { connect } from 'react-redux';
+import HumidityMonitor from '../../components/Monitors/HumidityMonitor/HumidityMonitor';
 import TemperatureMonitor from '../../components/Monitors/TemperatureMonitor/TemperatureMonitor';
 import { msToMinutes } from '../../utils/time.utils';
 
@@ -10,18 +11,16 @@ const Dashboard = () => {
     return (
         <div className="Dashboard">
             <div className='page-header'>
-                    <Title style={{ fontSize: "48px", margin: 0, color: "white" }} level={1}>Dashboard</Title>
-                </div>
+                <Title style={{ fontSize: "48px", margin: 0, color: "white" }} level={1}>Dashboard</Title>
+            </div>
 
             <div style={{ margin: "10px auto", width: "94%" }}>
                 <Row gutter={[24, 24]} justify="space-evenly">
                     <Col xs={24} sm={24} md={12} lg={8} xl={6}>
-                        <TemperatureMonitor delay={fetchInterval} temperatureValue={36.5}/>
+                        <TemperatureMonitor delay={fetchInterval} />
                     </Col>
                     <Col xs={24} sm={24} md={12} lg={8} xl={6}>
-                        <div style={{ background: "blue", height: "300px", width: "100%" }}>
-                            Humidity
-                        </div>
+                        <HumidityMonitor delay={fetchInterval} />
                     </Col>
                     <Col xs={24} sm={24} md={12} lg={8} xl={6}>
                         <div style={{ background: "blue", height: "300px", width: "100%" }}>
