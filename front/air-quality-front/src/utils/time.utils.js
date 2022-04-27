f38392c443800b4d5fd7ab0568690f8249a8505f
mauduran <is716122@iteso.mx>
2022-04-26 23:21:33 -0500

    Add util function to time utils

--- front/air-quality-front/src/utils/time.utils.js
+++ front/air-quality-front/src/utils/time.utils.js
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 export const msToSeconds = (seconds) => {
     return seconds * 1000;
 }
@@ -6,3 +8,8 @@ export const msToMinutes = (minutes) => {
     return minutes * 60 * 1000;
 }
 
+export const generateEmptyArrayWithDaysInMonth = (month, year) => {
+    const daysInMonth = moment(`${month}/${year}`, "MM/YYYY").daysInMonth();
+
+    return new Array(daysInMonth).fill(0);
+}
