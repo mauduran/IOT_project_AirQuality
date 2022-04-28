1c388c81672ec523a25aa2a124b959e5ab005c86
mauduran <is716122@iteso.mx>
2022-04-28 02:08:39 -0500

    Created functions for monthly charts data transformation

--- front/air-quality-front/src/utils/sensors.utils.js
+++ front/air-quality-front/src/utils/sensors.utils.js
@@ -1,3 +1,6 @@
+import RiskLevelColors from "../constants/RiskLevelColors";
+import ALERT_LEVELS, { ALERT_LEVELS_TEXT } from "../constants/AirQualityLevels";
+
 export const reduceValueArrayToDayAverages = (array, daysInMonth) => {
     const hashmap = array.reduce((map, item) => {
         if (item.day in map) {
@@ -10,9 +13,28 @@ export const reduceValueArrayToDayAverages = (array, daysInMonth) => {
 
     const dayAveragesArray = new Array(daysInMonth).fill(0);
 
-    for(const [key, val] of Object.entries(hashmap)) {
-        dayAveragesArray[parseInt(key)-1] = val.reduce((a,b)=>a+b) / val.length;
+    for (const [key, val] of Object.entries(hashmap)) {
+        dayAveragesArray[parseInt(key) - 1] = val.reduce((a, b) => a + b) / val.length;
     }
 
     return dayAveragesArray;
+}
+
+export const reduceValueArrayToLevelCount = (array, daysInMonth) => {
+    const valueLevelCountMap = {}
+    for (const level of Object.keys(ALERT_LEVELS)) {
+        valueLevelCountMap[level] = {
+            color: RiskLevelColors[level],
+            title: ALERT_LEVELS_TEXT[level],
+            values: new Array(daysInMonth).fill(0)
+        }
+    }
+
+    for (const item of array) {
+        if (item.level in valueLevelCountMap) {
+            valueLevelCountMap[item.level].values[parseInt(item.day) - 1]++;
+        }
+    }
+
+    return valueLevelCountMap;
 }
